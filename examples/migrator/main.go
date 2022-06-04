961d51c3dd581065bfdf5097871444e2885e6a95
Mark Cooper <mark.c.cooper@outlook.com>
2022-06-04 13:22:00 -0700

    Chip away at ARM example

--- examples/migrator/main.go
+++ examples/migrator/main.go
@@ -9,19 +9,26 @@ import (
 	"github.com/mark-cooper/asclient"
 )
 
+type APIResourcesRefResponse struct {
+	Refs []struct {
+		Ref string `json:"ref"`
+	} `json:"resources"`
+}
+
 func main() {
 	srcClient, srcRepo := Setup(
 		"https://test.archivesspace.org/staff/api", "admin", "admin", "YNHSC",
 	)
 	fmt.Printf("Source: %v\n", srcRepo)
 
-	_, dstRepo := Setup(
-		"https://test.archivesspace.org/staff/api", "admin", "admin", "PlusOne",
+	dstClient, dstRepo := Setup(
+		"http://localhost:4567", "admin", "admin", "TEST",
 	)
 	fmt.Printf("Destination: %v\n", dstRepo)
 
-	ids := FetchIds(srcClient, srcRepo)
+	ids := FetchResourceIds(srcClient, srcRepo)
 
+	// Process resource ids
 	for _, id := range ids {
 		resp, err := srcClient.Get(
 			fmt.Sprintf("%s/resources/%d", srcRepo.URI, id), asclient.QueryParams{},
@@ -31,11 +38,44 @@ func main() {
 			continue
 		}
 
-		fmt.Println(resp.String())
+		var resource asclient.Resource
+		json.Unmarshal([]byte(resp.String()), &resource)
+
+		// Move along if the resource is unpublished
+		if !resource.Publish {
+			continue
+		}
+
+		// Lookup the resource in destination
+		identifier := srcClient.Identify(resource)
+		identifierLookup := fmt.Sprintf("[\"%s\"]", identifier) // reformat for ASpace api
+
+		resp, err = dstClient.Get(fmt.Sprintf("%s/find_by_id/resources", dstRepo.URI), asclient.QueryParams{
+			Identifier: []string{identifierLookup},
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		var refs APIResourcesRefResponse
+		json.Unmarshal([]byte(resp.String()), &refs)
+
+		// If the (updated) resource already exists we need to delete it first (no overlays)
+		if len(refs.Refs) > 0 {
+			fmt.Printf("Deleting existing record %v: %v\n", identifierLookup, refs.Refs[0].Ref)
+			_, err = dstClient.Delete(refs.Refs[0].Ref)
+
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		}
 	}
 }
 
-func FetchIds(client *asclient.APIClient, repo *asclient.Repository) []int {
+func FetchResourceIds(client *asclient.APIClient, repo *asclient.Repository) []int {
 	resp, err := client.Get(fmt.Sprintf("%s/resources", repo.URI), asclient.QueryParams{
 		AllIds:        true,
 		ModifiedSince: asclient.ModifiedSince(time.Hour * (24 * 7)),
--- model.go
+++ model.go
@@ -1,5 +1,7 @@
 package asclient
 
+import "strings"
+
 type Collection[T any] struct {
 	Records []T
 }
@@ -16,6 +18,28 @@ type Repository struct {
 	URI         string `json:"uri"`
 }
 
+type Resource struct {
+	LockVersion int    `json:"lock_version"`
+	Title       string `json:"title"`
+	Publish     bool   `json:"publish"`
+	EadID       string `json:"ead_id"`
+	ID0         string `json:"id_0"`
+	ID1         string `json:"id_1"`
+	ID2         string `json:"id_2"`
+	ID3         string `json:"id_3"`
+	URI         string `json:"uri"`
+}
+
 func (repository Repository) FingerPrint() string {
 	return repository.RepoCode
+}
+
+func (resource Resource) FingerPrint() string {
+	var parts []string
+	for _, s := range []string{resource.ID0, resource.ID1, resource.ID2, resource.ID3} {
+		if strings.TrimSpace(s) != "" {
+			parts = append(parts, s)
+		}
+	}
+	return strings.Join(parts, ".")
 }
